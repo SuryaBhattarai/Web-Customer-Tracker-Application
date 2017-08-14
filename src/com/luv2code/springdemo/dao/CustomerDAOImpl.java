d15a1deaa747656478c571c59e0a7db8a075f879
SuryaBhattarai <surya.pr.bhattarai@gmail.com>
2017-08-14 11:06:42 +0200

    first

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,62 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query... sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+		
+		// execute query and  get the result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		
+		// return the result
+		return customers;
+	}
+
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save/ update the customer..... finally
+		currentSession.saveOrUpdate(theCustomer);
+		}
+
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// mow retrieve / read form database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		
+		return theCustomer;
+	}
+
+}
